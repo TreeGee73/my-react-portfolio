58b5d60b1e00bca3edde0caa433330bfd50babbf
Theresa <TreeGee73@gmail.com>
2021-01-23 20:55:01 -0700

    implimented to framework, added files

---
+++ src/components/about.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Us</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am a certified full stack developer, with an Associates of Science degree in Information Techonology with a Visual Communications concentration and 10 years of front end web development experience applied in a variety of industries.</p>
+                    <p>Besides being passionate about web development, I enjoy a multitude of other things but my family always comes first. I don't know if it's something that is shared with other folks who enjoy programming, but I am an avid cook and baker. I enjoy spending time in the kitchen creating new dishes or baking (what can be elaborate) confections.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development</h3>
+                    <p>I have experience building websites using HTML, CSS, JavaScript, and React. <br /><br /></p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Full-Stack Development</h3>
+                    <p>I have successfully developed applications that utilized MySQL and MongoDB, am currently seeking an opportunity to expand my experience.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I have experience with mobile app development, email marketing (creative and campaign development), and social media marketing.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>I have successfully created printed and digital products using Adobe inDesign. I am also pretty handy with Photoshop and can manipulate images and create graphics when needed.</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
